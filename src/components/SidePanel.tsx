8c02e4623b479a97e6c19ef3947b7e390c9636a2
nikhilmateAllen <nikhil.mate@allen.in>
2025-03-07 23:51:03 +0530

    fix

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -18,8 +18,11 @@ export default function SidePanel({ activeTab, setActiveTab, tabList, user }: Si
   return (
     <div className="w-64 bg-white border-r">
       <div className="p-4">
-        <h2 className="text-xl px-6 py-3 border-2 border-solid border-blue-950 rounded-lg mb-4 text-blue-950 font-bold">ALLEN Connect</h2>
-        <h2 className="text-xl font-semibold mb-4">Welcome, {user?.name}</h2>
+        <div className="flex flex-col items-center">
+          <h2 className="text-xl px-4 leading-none py-2 border-2 border-solid border-blue-950 rounded-lg text-blue-950 font-bold">ALLEN Connect</h2>
+          <p className="text-base text-orange-700 text-center font-bold leading-none mt-1">Match! Learn! Grow!</p>
+        </div>
+        <h2 className="text-xl font-semibold mb-4 mt-4">Welcome, {user?.name}</h2>
         <h2 className="text-xl font-semibold mb-4">Filters</h2>
         <div className="space-y-2">
           {tabList.map((tab) => (
